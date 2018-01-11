067668231c8fdb64d01b5de60d7082096554607c
roilist7000 <roilist7000@mail.ru>
2018-01-11 23:00:43 +0300

    footer

--- footer.php
+++ footer.php
@@ -1,5 +1,33 @@
 <footer class="footer">
-
+    <div class="container hr_f">
+        <div class="navbar navbar-top row newnavbar" role="navigation">
+            <div class="navbar-header logo col-md-3 col-xs-12">
+                <a href="/">domik<span>nsk.ru</span></a>
+            </div>
+            <div class="menu col-md-6">
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="active"><a href="#">Каталог коттеджей</a></li>
+                    <li><a href="#">услуги для вашего отдыха</a></li>
+                </ul>
+            </div>
+            <div class="col-md-3 col-xs-12 phone">
+                <a href="tel:+7 (962) 82 63 707">+7 (962) 82 63 707</a>
+                <span>заказать обратный звонок</span>
+            </div>
+        </div>
+        <div class="row cop">
+            <div class="col-md-3 col-xs-12 soc">
+                <ul>
+                    <li><img src="/images/ins.png" alt="инстаграмм" title="инстаграмм"></li>
+                    <li><img src="/images/f.png" alt="facebook" title="facebook"></li>
+                </ul>
+            </div>
+            <div class="copyright col-md-5 col-md-offset-4 col-xs-12">
+                Коттедж "Здоровье" г. Новосибирск, Нижняя Ельцовка,
+                Садовое Общество "Здоровье"
+            </div>
+        </div>
+    </div>
 </footer>
 
 <div id="modalBasket" class="modal modal-basket fade" role="dialog">
