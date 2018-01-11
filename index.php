d4fc10700e04d64397487a9356661d189762c378
roilist7000 <roilist7000@mail.ru>
2018-01-12 00:20:37 +0300

    whhg

--- index.php
+++ index.php
@@ -2,7 +2,29 @@
 <div class="section-main">
     <div class="container">
         <div class="row">
-
+<div class="col-md-6 col-xs-12 left-main">
+    <h2>Коттедж «Здоровье»</h2>
+    <h3>Нижняя Ельцовка</h3>
+    <ul>
+        <li><i class="icon-circleright"></i>Вместимость до 100 человек</li>
+        <li><i class="icon-circleright">Количество спальных мест  25</li>
+        <li><i class="icon-circleright">от 17 000 руб.</li>
+    </ul>
+    <div class="new_row">
+        <div class="col-r in">
+            <a>Подробнее <i class="icon-circleright"></a>
+        </div>
+        <div class="col-r text-center map">
+            <a><i class="icon-map"></i> как проехать</a>
+        </div>
+        <div class="col-r text-right foto">
+            <a><i class="icon-map"></i><span>1<br>9</span></a>
+        </div>
+    </div>
+</div>
+            <div class="col-md-6 col-xs-12 right-main">
+<img src="/images/img01.png" alt="" title="">
+            </div>
         </div>
     </div>
 </div>
