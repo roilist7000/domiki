ccc22d6eee071cf405eb7ec2eba83c2249beaaa5
roilist7000 <roilist7000@mail.ru>
2018-01-13 00:29:24 +0300

    паыв

--- index.php
+++ index.php
@@ -152,6 +152,153 @@
                 </div>
                 <a class="href-s"><i class="icon-circleright"></i></a>
             </div>
+            <div class="col-md-6 col-xs-12 one-obj">
+                <img class="img-responsive" src="/images/one-obg.png" alt="" title="">
+                <a class="cal">
+                    <i class="icon-calendar"></i>
+                    <span>свободные даты</span>
+                </a>
+                <div class="bottom-obj">
+                    <div class="lef-b-obj">
+                        <a>коттедж название длинное</a>
+                        <div class="obj-dop">
+                            <ul><li><i class="icon-mouse"></i><span>10</span></li>
+                                <li><i class="icon-mouse"></i><span>40</span></li>
+                                <li><i class="icon-mouse"></i><span>15 000</span></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <i class="icon-mouse"></i>
+                    <i class="icon-mouse"></i>
+                </div>
+                <a class="href-s"><i class="icon-circleright"></i></a>
+            </div>
+            <div class="col-md-6 col-xs-12 one-obj">
+                <img class="img-responsive" src="/images/one-obg.png" alt="" title="">
+                <a class="cal">
+                    <i class="icon-calendar"></i>
+                    <span>свободные даты</span>
+                </a>
+                <div class="bottom-obj">
+                    <div class="lef-b-obj">
+                        <a>коттедж название длинное</a>
+                        <div class="obj-dop">
+                            <ul><li><i class="icon-mouse"></i><span>10</span></li>
+                                <li><i class="icon-mouse"></i><span>40</span></li>
+                                <li><i class="icon-mouse"></i><span>15 000</span></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <i class="icon-mouse"></i>
+                    <i class="icon-mouse"></i>
+                </div>
+                <a class="href-s"><i class="icon-circleright"></i></a>
+            </div>
+            <div class="col-md-6 col-xs-12 one-obj">
+                <img class="img-responsive" src="/images/one-obg.png" alt="" title="">
+                <a class="cal">
+                    <i class="icon-calendar"></i>
+                    <span>свободные даты</span>
+                </a>
+                <div class="bottom-obj">
+                    <div class="lef-b-obj">
+                        <a>коттедж название длинное</a>
+                        <div class="obj-dop">
+                            <ul><li><i class="icon-mouse"></i><span>10</span></li>
+                                <li><i class="icon-mouse"></i><span>40</span></li>
+                                <li><i class="icon-mouse"></i><span>15 000</span></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <i class="icon-mouse"></i>
+                    <i class="icon-mouse"></i>
+                </div>
+                <a class="href-s"><i class="icon-circleright"></i></a>
+            </div>
+            <div class="col-md-6 col-xs-12 one-obj">
+                <img class="img-responsive" src="/images/one-obg.png" alt="" title="">
+                <a class="cal">
+                    <i class="icon-calendar"></i>
+                    <span>свободные даты</span>
+                </a>
+                <div class="bottom-obj">
+                    <div class="lef-b-obj">
+                        <a>коттедж название длинное</a>
+                        <div class="obj-dop">
+                            <ul><li><i class="icon-mouse"></i><span>10</span></li>
+                                <li><i class="icon-mouse"></i><span>40</span></li>
+                                <li><i class="icon-mouse"></i><span>15 000</span></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <i class="icon-mouse"></i>
+                    <i class="icon-mouse"></i>
+                </div>
+                <a class="href-s"><i class="icon-circleright"></i></a>
+            </div>
+            <div class="col-md-6 col-xs-12 one-obj">
+                <img class="img-responsive" src="/images/one-obg.png" alt="" title="">
+                <a class="cal">
+                    <i class="icon-calendar"></i>
+                    <span>свободные даты</span>
+                </a>
+                <div class="bottom-obj">
+                    <div class="lef-b-obj">
+                        <a>коттедж название длинное</a>
+                        <div class="obj-dop">
+                            <ul><li><i class="icon-mouse"></i><span>10</span></li>
+                                <li><i class="icon-mouse"></i><span>40</span></li>
+                                <li><i class="icon-mouse"></i><span>15 000</span></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <i class="icon-mouse"></i>
+                    <i class="icon-mouse"></i>
+                </div>
+                <a class="href-s"><i class="icon-circleright"></i></a>
+            </div>
+            <div class="col-md-6 col-xs-12 one-obj">
+                <img class="img-responsive" src="/images/one-obg.png" alt="" title="">
+                <a class="cal">
+                    <i class="icon-calendar"></i>
+                    <span>свободные даты</span>
+                </a>
+                <div class="bottom-obj">
+                    <div class="lef-b-obj">
+                        <a>коттедж название длинное</a>
+                        <div class="obj-dop">
+                            <ul><li><i class="icon-mouse"></i><span>10</span></li>
+                                <li><i class="icon-mouse"></i><span>40</span></li>
+                                <li><i class="icon-mouse"></i><span>15 000</span></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <i class="icon-mouse"></i>
+                    <i class="icon-mouse"></i>
+                </div>
+                <a class="href-s"><i class="icon-circleright"></i></a>
+            </div>
+            <div class="col-md-6 col-xs-12 one-obj">
+                <img class="img-responsive" src="/images/one-obg.png" alt="" title="">
+                <a class="cal">
+                    <i class="icon-calendar"></i>
+                    <span>свободные даты</span>
+                </a>
+                <div class="bottom-obj">
+                    <div class="lef-b-obj">
+                        <a>коттедж название длинное</a>
+                        <div class="obj-dop">
+                            <ul><li><i class="icon-mouse"></i><span>10</span></li>
+                                <li><i class="icon-mouse"></i><span>40</span></li>
+                                <li><i class="icon-mouse"></i><span>15 000</span></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <i class="icon-mouse"></i>
+                    <i class="icon-mouse"></i>
+                </div>
+                <a class="href-s"><i class="icon-circleright"></i></a>
+            </div>
         </div>
         <div class="row top-line">
             <div class="breadcrumbs col-md-8 col-md-offset-4 col-xs-12 text-right new-br">
@@ -166,6 +313,18 @@
                     <li><a>7</a></li>
                     <li><a><i class="icon-arrow-right"></i></a></li>
                 </ul>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="section-contact">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+    <span class="text-center">нужен вариант для отдыха? Звони — подберем!</span>
+    <p class="text-center">По любым вопросам звоните нам<br>
+        ежедневно с 10:00 до 23:00</p>
+    <a href="tel:+7 (962) 82 63 707">+7 (962) 82 63 707</a>
             </div>
         </div>
     </div>
