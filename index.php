dde3bd9a354099cadbbe5792961f79784c5a4a64
roilist7000 <roilist7000@mail.ru>
2018-01-12 22:05:12 +0300

    dp

--- index.php
+++ index.php
@@ -29,5 +29,85 @@
             </div>
         </div>
     </div>
-</div>
+
+    <div class="section-two-main">
+        <div class="container">
+            <div class="row">
+                <h1>Коттедж, известный каждому, в Новосибирске
+                    <span>с бассейном!</span></h1>
+            </div>
+            <div class="row rowline">
+                <div class="col-md-1 col-xs-3 one_prem_main">
+                    <span>много
+<br>места</span><i class="icon-automobile-car"></i>
+                </div>
+                <div class="col-md-1 col-xs-3 one_prem_main">
+                    <span>много
+<br>места</span><i class="icon-automobile-car"></i>
+                </div>
+                <div class="col-md-1 col-xs-3 one_prem_main">
+                    <span>много
+<br>места</span><i class="icon-automobile-car"></i>
+                </div>
+                <div class="col-md-1 col-xs-3 one_prem_main">
+                    <span>много
+<br>места</span><i class="icon-automobile-car"></i>
+                </div>
+                <div class="col-md-1 col-xs-3 one_prem_main">
+                    <span>много
+<br>места</span><i class="icon-automobile-car"></i>
+                </div>
+                <div class="col-md-1 col-xs-3 one_prem_main">
+                    <span>много
+<br>места</span><i class="icon-automobile-car"></i>
+                </div>
+                <div class="col-md-1 col-xs-3 one_prem_main">
+                    <span>много
+<br>места</span><i class="icon-automobile-car"></i>
+                </div>
+                <div class="col-md-1 col-xs-3 one_prem_main">
+                    <span>много
+<br>места</span><i class="icon-automobile-car"></i>
+                </div>
+                <div class="col-md-1 col-xs-3 one_prem_main">
+                    <span>много
+<br>места</span><i class="icon-automobile-car"></i>
+                </div>
+                <div class="col-md-1 col-xs-3 one_prem_main">
+                    <span>много
+<br>места</span><i class="icon-automobile-car"></i>
+                </div>
+                <div class="col-md-1 col-xs-3 one_prem_main">
+                    <span>много
+<br>места</span><i class="icon-automobile-car"></i>
+                </div>
+                <div class="col-md-1 col-xs-3 one_prem_main">
+                    <span>свадьба</span><i class="icon-automobile-car"></i>
+                </div>
+            </div>
+            <div class="row row-p">
+                <div class="col-md-4 col-xs-12 one-p">
+                    <img src="/images/one-p.png" alt="" title="">
+                    <p>Специально укомплектованный дом для проведения различных мероприятий, таких как: Дни Рождения, Свадьбы, Корпоративы, Дискотеки и любые другие памятные даты.
+                        Большая вместимость,
+                        много парковочных мест</p>
+                </div>
+                <div class="col-md-4 col-xs-12 one-p">
+                    <img src="/images/two-p.png" alt="" title="">
+                    <p>Уникальный бассейн с подогревом оригинальной формы и работающий КРУГЛЫЙ ГОД!</p>
+                </div>
+                <div class="col-md-4 col-xs-12 one-p">
+                    <img src="/images/three-p.png" alt="" title="">
+                    <p>Сауна, джакузи, мангал и казан в доме, банкетные столы,  акустическая система, интернет,кинопроектор с большим экраном, большой бильярдный стол с каменной столешницей, профессиональный настольный мини футбол.</p>
+                </div>
+            </div>
+            <div class="row row-b">
+                <div class="col-md-8 col-md-offset-1 col-xs-12 yel-b">
+                    <span> Если Вы хотите снять коттедж на сутки в Новосибирске
+– лучшего варианта не найти!</span>
+                    <a>Подробнее <i class="icon-circleright"></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
 <? require("footer.php"); ?>
