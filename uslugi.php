444394aa2661cf779562ff55883cb1f65b357718
roilist7000 <roilist7000@mail.ru>
2018-01-13 18:13:01 +0300

    uslugi

---
+++ uslugi.php
@@ -0,0 +1,58 @@
+<? require("header.php"); ?>
+<div class="section-usugi">
+    <div class="container">
+        <div class="row uslugi">
+           <h1>услуги для вашего отдыха</h1>
+            <h3>Отметте все, что вам необходимо, и подберите идеальный вариант</h3>
+            <form>
+                <div class="col-md-4 col-xs-12 prem-menu">
+                    <div class="prem-block">
+                        <div class="title-prem">
+                            <i class="icon-mouse"></i>
+                            <div>
+                                <h4>Бассейн</h4>
+                                <span>найдено 2 варианта <a>показать все</a></span>
+                            </div>
+                        </div>
+                        <div class="prem-pr">
+                            <div class="col-md-6 col-xs-12 one-prem">
+                                <a>
+                                    <img src="/images/one-prem.png" alt="" title="">
+                                    <i class="icon-circleright"></i>
+                                </a>
+                                <span>
+                                    <a>Название коттеджа<br/>
+                                        аренда от 20 000 руб.</a>
+                                        </span>
+                            </div>
+                            <div class="col-md-6 col-xs-12 one-prem">
+                                <a>
+                                    <img src="/images/ne-prem.png" alt="" title="">
+                                    <i class="icon-circleright"></i>
+                                </a>
+                                <span>
+                                    <a>Название коттеджа<br/>
+                                        аренда от 20 000 руб.</a>
+                                        </span>
+                            </div>
+                        </div>
+                    </div>
+                    <p>Коттедж известный каждому в Новосибирске с бассейном! Специально укомплектованный дом для проведения различных мероприятий, таких как: Дни Рождения, Свадьбы, Корпоративы,  Дискотеки и любые другие памятные даты. Уникальный бассейн с подогревом оригинальной формы и работающий КРУГЛЫЙ ГОД! Сауна, джакузи, мангал и казан в доме, банкетные столы,  акустическая система, интернет,кинопроектор с большим экраном, большой бильярдный стол с каменной столешницей, профессиональный настольный мини футбол, много парковочных мест. Если Вы хотите снять коттедж на сутки в Новосибирске – лучшего варианта не найти.</p>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+<div class="section-contact">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+    <span class="text-center">нужен вариант для отдыха? Звони — подберем!</span>
+    <p class="text-center">По любым вопросам звоните нам<br>
+        ежедневно с 10:00 до 23:00</p>
+    <a href="tel:+7 (962) 82 63 707">+7 (962) 82 63 707</a>
+            </div>
+        </div>
+    </div>
+</div>
+<? require("footer.php"); ?>
