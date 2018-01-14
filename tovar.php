11e299e011f9f7edfb507dda04cec5315ff21767
roilist7000 <roilist7000@mail.ru>
2018-01-14 15:34:08 +0300

    tovar

--- tovar.php
+++ tovar.php
@@ -41,7 +41,143 @@
                     </div>
                 </div>
             </div>
+<div class="col-md-6 col-xs-12 right-t">
+    <div class="price-top">
+    <i class="icon-roublealt"></i>
+    <span>
+        <strong>Стоимость аренды</strong> (сутки/ в рублях)
+        </span>
+    </div>
+    <div class="price-bottom">
+        <div class="pn">
+            <span><strong>Пн-Чт</strong>
+15 000</span>
+        </div>
+        <div class="pn">
+            <span><strong>Пт</strong>
+25 000</span>
+        </div>
+        <div class="pn pr-h">
+            <span><strong>Сб</strong>
+25 000</span>
+        </div>
+        <div class="pn pr-h">
+            <span><strong>Вс</strong>
+25 000</span>
+        </div>
+        <div class="pn new-y">
+            <i class="icon-forest-tree"></i><span><strong>Новый год</strong>
+150 000</span>
+        </div>
 
+    </div>
+    <div class="row row-do">
+        <div class="col-md-6 col-xs-12 line-c">
+<a class="b-but"><span>бронировать</span><i class="icon-tagalt-pricealt"></i></a>
+        </div>
+        <div class="col-xs-12 col-md-6 line-otz">
+            <div class="reit">
+                <span>Рейтинг<br>отдыхающих</span>
+                <span class="num-reit">4,25</span>
+            </div>
+            <div class="full-reit">
+                <div class="one-reit">
+                    <span>
+                        5
+                        <strong>чистота</strong>
+                    </span>
+                </div>
+                <div class="one-reit">
+                    <span>
+                        5
+                        <strong>ремонт</strong>
+                    </span>
+                </div>
+                <div class="one-reit">
+                    <span>
+                        5
+                        <strong>сервис</strong>
+                    </span>
+                </div>
+            </div>
+            <div class="num-otz">
+                <i class="icon-hangouts"></i><span>Количество отзывов - 1</span>
+            </div>
+            <div class="guests">
+                <span>ПОЧЕМУ ВАЖНО ОСТАВЛЯТЬ ОТЗЫВЫ?</span>
+<p>
+Поделившись своим опытом,
+Вы помогаете другим отдыхающим
+сделать правильный выбор
+и составить план идеального отдыха.
+</p>
+                <a class="but-otz"><span>оставить отзыв</span><i class="icon-hangouts"></i></a>
+            </div>
+        </div>
+    </div>
+</div>
+        </div>
+        <div class="row row-tmenu">
+            <div class="col-xs-12">
+            <ul>
+                <li class="active" dara-tab="desc">описание</li>
+                <li dara-tab="otz">отзывы</li>
+                <li dara-tab="foto">все фото</li>
+                <li dara-tab="maps">карта</li>
+            </ul>
+            </div>
+        </div>
+        <div class="row row-tab active rowdesc">
+<div class="col-xs-12">
+    <div class="desc-ttv">
+<p>Коттедж известный каждому в Новосибирске с бассейном! Специально укомплектованный дом для проведения различных мероприятий, таких как: Дни Рождения, Свадьбы, Корпоративы,  Дискотеки и любые другие памятные даты. Уникальный бассейн с подогревом оригинальной формы и работающий КРУГЛЫЙ ГОД! Сауна, джакузи, мангал и казан в доме, банкетные столы,  акустическая система, интернет,кинопроектор с большим экраном, большой бильярдный стол с каменной столешницей, профессиональный настольный мини футбол, много парковочных мест. Если Вы хотите снять коттедж на сутки в Новосибирске – лучшего варианта не найти.</p>
+        <div class="desc-sp">
+            <div class="desc-sp-left">
+                <ul>
+                    <li><i class="icon-automobile-car"></i><span>Вместимость до 100 человек</span></li>
+                    <li><i class="icon-automobile-car"></i><span>Профессиональный настольный
+мини футбол</span></li>
+                    <li><i class="icon-automobile-car"></i><span>Барная стойка с возможностью
+подключения 2 пивных кег
+и охладителем для них (2 крана)</span></li>
+                    <li><i class="icon-automobile-car"></i><span>Вместимость до 100 человек</span></li>
+                    <li><i class="icon-automobile-car"></i><span>Профессиональный настольный
+мини футбол</span></li>
+                    <li><i class="icon-automobile-car"></i><span>Барная стойка с возможностью
+подключения 2 пивных кег
+и охладителем для них (2 крана)</span></li>
+                    <li><i class="icon-automobile-car"></i><span>Вместимость до 100 человек</span></li>
+                    <li><i class="icon-automobile-car"></i><span>Профессиональный настольный
+мини футбол</span></li>
+                    <li><i class="icon-automobile-car"></i><span>Барная стойка с возможностью
+подключения 2 пивных кег
+и охладителем для них (2 крана)</span></li>
+                    <li><i class="icon-automobile-car"></i><span>Вместимость до 100 человек</span></li>
+                    <li><i class="icon-automobile-car"></i><span>Профессиональный настольный
+мини футбол</span></li>
+                    <li><i class="icon-automobile-car"></i><span>Барная стойка с возможностью
+подключения 2 пивных кег
+и охладителем для них (2 крана)</span></li>
+                    <li><i class="icon-automobile-car"></i><span>Вместимость до 100 человек</span></li>
+                    <li><i class="icon-automobile-car"></i><span>Профессиональный настольный
+мини футбол</span></li>
+                    <li><i class="icon-automobile-car"></i><span>Барная стойка с возможностью
+подключения 2 пивных кег
+и охладителем для них (2 крана)</span></li>
+                    <li><i class="icon-automobile-car"></i><span>Вместимость до 100 человек</span></li>
+                    <li><i class="icon-automobile-car"></i><span>Профессиональный настольный
+мини футбол</span></li>
+                    <li><i class="icon-automobile-car"></i><span>Барная стойка с возможностью
+подключения 2 пивных кег
+и охладителем для них (2 крана)</span></li>
+                </ul>
+            </div>
+            <div class="desc-sp-right">
+
+            </div>
+        </div>
+    </div>
+</div>
         </div>
     </div>
 </div>
