a811d328b0eaf9f4ba978c50a4b51c454ee4e55f
roilist7000 <roilist7000@mail.ru>
2018-01-18 01:33:30 +0300

    календарь

---
+++ step03.php
@@ -0,0 +1,60 @@
+<? require("header.php"); ?>
+    <div class="section-main">
+    </div>
+<div class="section-tovar sect-step02">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-9 col-xs-12">
+                <h1>Коттедж «Здоровье»</h1>
+                <h2>Нижняя Ельцовка</h2>
+                <h1>Петр Степанович</h1>
+                <h2><a>+7931999999</a></h2>
+            </div>
+            <div class="col-md-3 col-xs-12 ret-u">
+                <a><span>вернуться в каталог</span> <i class="icon-circleright"></i></a>
+            </div>
+        </div>
+        <div class="row tovar-top">
+<div class="col-md-12 col-xs-12 right-t">
+    <div class="row row-do">
+        <div class="col-md-3 col-xs-12 line-c">
+            <div class="sv-date">
+                Редактирование
+            </div>
+            <div id="calendar_table"></div>
+            <span>Забронировано</span>
+            <span class="bbbt">Текущий заказ</span>
+            <a class="b-but"><span>Сохранить</span><i class="icon-tagalt-pricealt"></i></a>
+            <a class="b-but bt-02"><span>Удалить</span><i class="icon-tagalt-pricealt"></i></a>
+            <div class="indate" style="display: none" data-b="1812018">
+                <input type="hidden" class="b-date y-day-26-1-2018" name="date[]" value="day-26-1-2018">
+                <input type="hidden" class="b-date y-day-27-1-2018" name="date[]" value="day-27-1-2018">
+            </div>
+        </div>
+        <!-- <div class="col-xs-12 col-md-9 line-sp">
+  <div class="row line-z">
+      <div class="col-md-6 col-xs-12 cont-step">
+
+      </div>
+      <div class="col-md-2 col-xs-12 cont-date">
+          <span>26-1-2018</span>
+          <span>27-1-2018</span>
+      </div>
+      <div class="col-md-4 col-xs-12 cont-but">
+          <form>
+          <input type="submit" class="btn btn-success izmn" value="Сохранить">
+          </form>
+          <form>
+              <input type="submit" class="btn btn-warning" value="Удалить">
+          </form>
+      </div>
+  </div>
+          </div>-->
+    </div>
+</div>
+        </div>
+
+    </div>
+</div>
+
+<? require("footer.php"); ?>
